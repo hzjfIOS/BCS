fc2e4225677b2e4e3431afa8953bcfbd06ce515f
王祥 <314250993@qq.com>
2017-04-13 15:23:34 +0800

    Revert "错误修改"
    This reverts commit 82b528275b66ee7f657a1d4724eb8e6802118a5a.

--- Pods/Target Support Files/Pods-srph/Pods-srph-resources.sh
+++ Pods/Target Support Files/Pods-srph/Pods-srph-resources.sh
@@ -78,11 +78,9 @@ EOM
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "AliPay/AlipaySDK.bundle"
-  install_resource "SMSSDK/SMSSDKUI.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "AliPay/AlipaySDK.bundle"
-  install_resource "SMSSDK/SMSSDKUI.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
